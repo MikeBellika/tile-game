a4a7773ae221da1a6aa489723aa680c33f9876ed
Mike Bellika <mike@bellika.dk>
2024-03-11 13:42:39 -0400

    Remove semi colons from end of lines and other changes

--- components/Game.tsx
+++ components/Game.tsx
@@ -1,43 +1,56 @@
-"use client";
-import { Board, Position, useBoard } from "@/hooks/useBoard";
-import { motion, AnimatePresence } from "framer-motion";
-import { Fragment, useState } from "react";
+"use client"
+import { Board, Position, useBoard } from "@/hooks/useBoard"
+import { motion, AnimatePresence } from "framer-motion"
+import { useState } from "react"
 
 export default function Game() {
-  const { board: initialBoard, swapTile, getTileColor } = useBoard(8);
-  const [board, setBoard] = useState<Board>(initialBoard);
-  const [animating, setAnimating] = useState(false);
+  const {
+    board: initialBoard,
+    swapTile,
+    getTileColor,
+    getTileValue,
+  } = useBoard(8)
+  const [board, setBoard] = useState<Board>(initialBoard)
+  const [animating, setAnimating] = useState(false)
   const [selectedFrom, setSelectedFrom] = useState<Position | undefined>(
     undefined,
-  );
+  )
 
   async function clickTile(position: Position) {
     if (animating) {
-      return;
+      return
     }
     if (!selectedFrom) {
-      setSelectedFrom(position);
-      return;
+      console.log(getTileValue(position, board))
+      // return
+      setSelectedFrom(position)
+      return
     }
-    const boards = swapTile(selectedFrom, position, board);
-    setAnimating(true);
+    const boards = swapTile(selectedFrom, position, board)
+    setAnimating(true)
     for (const [index, newBoard] of boards.entries()) {
-      setBoard(newBoard);
+      setBoard(newBoard)
       if (index < boards.length - 1) {
-        await new Promise((r) => setTimeout(r, 700));
+        await new Promise((r) => setTimeout(r, 2000))
       }
     }
-    setSelectedFrom(undefined);
-    setAnimating(false);
+    setSelectedFrom(undefined)
+    setAnimating(false)
   }
   return (
     <main className="grid w-fit grid-cols-8 grid-rows-8 items-center gap-4 p-24">
-      <AnimatePresence>
+      <AnimatePresence mode="popLayout">
         {board.map((row, x) =>
           row.map((tile, y) => (
             <motion.button
               layout
-              transition={{ type: "spring", stiffness: 700, damping: 30 }}
+              transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 30,
+              }}
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
               key={tile.id}
               onClick={(_) => clickTile({ x, y })}
               style={{ background: getTileColor(tile) }}
@@ -49,5 +62,5 @@ export default function Game() {
         )}
       </AnimatePresence>
     </main>
-  );
+  )
 }
--- hooks/useBoard.ts
+++ hooks/useBoard.ts
@@ -1,24 +1,51 @@
-export type Tile = { id: number; value: number };
-export type Board = Tile[][];
-export type Position = { x: number; y: number };
+import { useMemo } from "react"
+
+export type Tile = { id: number; value: number }
+export type Board = Tile[][]
+export type Position = { x: number; y: number }
 
 function getRandomTileId(): number {
-  return Math.random();
+  return Math.random()
+}
+
+function getRandomTileValue(): number {
+  return Math.floor(Math.random() * 4) + 1
+}
+
+function getRandomTile(): Tile {
+  return {
+    id: getRandomTileId(),
+    value: getRandomTileValue(),
+  }
 }
 
 function generateBoard(size: number): Board {
-  return Array.from({ length: size }).map((_) =>
+  const iniialBoard = Array.from({ length: size }).map((_) =>
     Array.from({ length: size }).map((__) => {
-      return {
-        id: getRandomTileId(),
-        value: Math.floor(Math.random() * 4) + 1,
-      };
+      return getRandomTile()
     }),
-  );
+  )
+  let changedValue = true
+  while (changedValue) {
+    changedValue = false
+    for (let x = 0; x < size; x++) {
+      for (let y = 0; y < size; y++) {
+        const tileValue = getTileValue({ x, y }, iniialBoard).value
+        if (tileValue != iniialBoard[x][y].value) {
+          iniialBoard[x][y] = {
+            ...iniialBoard[x][y],
+            value: getRandomTileValue(),
+          }
+          changedValue = true
+        }
+      }
+    }
+  }
+  return iniialBoard
 }
 
 function copyBoard(board: Board): Board {
-  return board.slice().map((column) => column.slice());
+  return board.slice().map((column) => column.slice())
 }
 
 function isMoveValid(
@@ -27,105 +54,148 @@ function isMoveValid(
   before: Board,
   after: Board,
 ): boolean {
-  const fromBefore = getTileValue(from, before);
-  const fromAfter = getTileValue(from, after);
-  const toBefore = getTileValue(to, before);
-  const toAfter = getTileValue(to, after);
+  const fromBefore = getTileValue(from, before).value
+  const fromAfter = getTileValue(to, after).value
+  const toBefore = getTileValue(to, before).value
+  const toAfter = getTileValue(from, after).value
   // If either tile value changed between moves, it's a valid move
-  return fromBefore != fromAfter || toBefore != toAfter;
+  return fromBefore != fromAfter || toBefore != toAfter
 }
 
-function getAmountOfSameTilesUp(position: Position, board: Board): number {
-  let result = 0;
-  const tile = board[position.x][position.y];
-  for (let y = position.y; y > 0; y--) {
+function getSameTilesUp(position: Position, board: Board): Array<Position> {
+  let result: Array<Position> = []
+  const tile = board[position.x][position.y]
+  for (let y = position.y - 1; y > 0; y--) {
     if (board[position.x][y].value != tile.value) {
-      break;
+      break
     }
-    result++;
+    result = [...result, { x: position.x, y }]
   }
-  return result >= 2 ? result : 0;
+  return result
 }
-function getAmountOfSameTilesDown(position: Position, board: Board): number {
-  let result = 0;
-  const tile = board[position.x][position.y];
-  for (let y = position.y; y < board[position.x].length; y++) {
+function getSameTilesDown(position: Position, board: Board): Array<Position> {
+  let result: Array<Position> = []
+  const tile = board[position.x][position.y]
+  for (let y = position.y + 1; y < board[position.x].length; y++) {
     if (board[position.x][y].value != tile.value) {
-      break;
+      break
     }
-    result++;
+    result = [...result, { x: position.x, y }]
   }
-  return result >= 2 ? result : 0;
-}
-function getAmountOfSameTilesLeft(position: Position, board: Board): number {
-  let result = 0;
-  const tile = board[position.x][position.y];
-  for (let x = position.x; x > 0; x--) {
-    if (board[x][position.y].value != tile.value) {
-      break;
-    }
-    result++;
-  }
-  return result >= 2 ? result : 0;
-}
-function getAmountOfSameTilesRight(position: Position, board: Board): number {
-  let result = 0;
-  const tile = board[position.x][position.y];
-  for (let x = position.x; x < board.length; x++) {
-    if (board[x][position.y].value != tile.value) {
-      break;
-    }
-    result++;
-  }
-  return result >= 2 ? result : 0;
+  return result
 }
 
-function getTileValue(position: Position, board: Board): number {
-  const tile = board[position.x][position.y];
-
-  const pointsUp = getAmountOfSameTilesUp(position, board);
-  const pointsDown = getAmountOfSameTilesDown(position, board);
-  const pointsLeft = getAmountOfSameTilesLeft(position, board);
-  const pointsRight = getAmountOfSameTilesRight(position, board);
-  const points = pointsUp + pointsDown + pointsLeft + pointsRight;
-  if (points == 0) {
-    return tile.value;
+function getSameTilesLeft(position: Position, board: Board): Array<Position> {
+  let result: Array<Position> = []
+  const tile = board[position.x][position.y]
+  for (let x = position.x - 1; x > 0; x--) {
+    // Adjust boundary check to >= 0
+    if (board[x][position.y].value != tile.value) {
+      break
+    }
+    result = [...result, { x, y: position.y }]
   }
-  return tile.value * Math.pow(2, points - 1);
+  return result
+}
+
+function getSameTilesRight(position: Position, board: Board): Array<Position> {
+  let result: Array<Position> = []
+  const tile = board[position.x][position.y]
+  for (let x = position.x + 1; x < board.length; x++) {
+    if (board[x][position.y].value != tile.value) {
+      break
+    }
+    result = [...result, { x, y: position.y }]
+  }
+  return result
+}
+
+function getTileValue(
+  position: Position,
+  board: Board,
+): { value: number; matchedTiles: Array<Position> } {
+  console.log("getTileValue called")
+  const tile = board[position.x][position.y]
+
+  const tilesUp = getSameTilesUp(position, board)
+  const tilesDown = getSameTilesDown(position, board)
+  const tilesLeft = getSameTilesLeft(position, board)
+  const tilesRight = getSameTilesRight(position, board)
+  // console.log({ position, tilesUp, tilesDown, tilesLeft, tilesRight })
+  const points =
+    tilesUp.length + tilesDown.length + tilesLeft.length + tilesRight.length
+  if (points < 2) {
+    return { value: tile.value, matchedTiles: [] }
+  }
+  return {
+    value: tile.value + points - 1,
+    matchedTiles: [...tilesUp, ...tilesDown, ...tilesRight, ...tilesLeft],
+  }
 }
 
 /**
  * The main thing. Returns a list of boards to be animated through
  */
 function swapTile(from: Position, to: Position, board: Board): Board[] {
-  const newBoard = copyBoard(board);
-  newBoard[to.x][to.y] = board[from.x][from.y];
-  newBoard[from.x][from.y] = board[to.x][to.y];
-  if (true || !isMoveValid(from, to, board, newBoard)) {
-    console.log("move not valid");
+  const newBoard = copyBoard(board)
+  newBoard[to.x][to.y] = board[from.x][from.y]
+  newBoard[from.x][from.y] = board[to.x][to.y]
+  if (!isMoveValid(from, to, board, newBoard)) {
+    console.log("move not valid")
     // Animate to the swapped board, then back again
-    return [newBoard, board];
+    return [newBoard, board]
   }
-  const fromNewValue = getTileValue(from, board);
-  const toNewValue = getTileValue(to, board);
+  const fromNewValue = getTileValue(from, newBoard)
+  const toNewValue = getTileValue(to, newBoard)
   newBoard[from.x][from.y] = {
     ...newBoard[from.x][from.y],
-    value: fromNewValue,
-  };
-  newBoard[to.x][to.y] = { ...newBoard[to.x][to.y], value: toNewValue };
-  // TODO: Do this in a loop
-  return [board, newBoard];
+    value: fromNewValue.value,
+  }
+  newBoard[to.x][to.y] = { ...newBoard[to.x][to.y], value: toNewValue.value }
+
+  const matchedTiles = [
+    ...fromNewValue.matchedTiles,
+    ...toNewValue.matchedTiles,
+  ]
+  const newBoardsFromMatchedTiles = boardFromMatchedTiles(
+    matchedTiles,
+    newBoard,
+  )
+  return [newBoard, ...newBoardsFromMatchedTiles]
+}
+
+function boardFromMatchedTiles(positions: Position[], board: Board): Board[] {
+  const copiedBoard = copyBoard(board)
+  let result = [copiedBoard]
+  for (const position of positions) {
+    copiedBoard[position.x][position.y] = getRandomTile()
+    const newTileValue = getTileValue({ ...position }, copiedBoard)
+    if (newTileValue.matchedTiles.length > 0) {
+      const newBoard = copyBoard(copiedBoard)
+      newBoard[position.x][position.y] = {
+        ...newBoard[position.x][position.y],
+        value: newTileValue.value,
+      }
+      result = [
+        ...result,
+        ...boardFromMatchedTiles(newTileValue.matchedTiles, newBoard),
+      ]
+    }
+  }
+  return result
 }
 
 function getTileColor(tile: Tile) {
   if (tile.value > 11) {
-    return "#000000";
+    return "#000000"
   }
-  return `hsl(${tile.value * 36} 100% 50%)`;
+  return `hsl(${tile.value * 36} 100% 50%)`
 }
 
 export function useBoard(size: number) {
-  const board: Board = generateBoard(size);
+  // Memoize board to prevent regeneration on every rerender,
+  // regenerate only if `size` changes
+  const board: Board = useMemo(() => generateBoard(size), [size])
 
-  return { board, swapTile, getTileColor };
+  return { board, swapTile, getTileColor, getTileValue }
 }
