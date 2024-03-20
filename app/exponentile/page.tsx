5c900115589956547b50bbb620f56381b69f99e9
Mike Bellika <mike@bellika.dk>
2024-03-20 19:48:52 +0100

    Add game to /exponentile path

---
+++ app/exponentile/page.tsx
@@ -0,0 +1,13 @@
+import dynamic from "next/dynamic"
+
+export default function Home() {
+  // To client side render the game
+  const Game = dynamic(() => import("../../components/Game"), {
+    ssr: false,
+  })
+  return (
+    <div className="flex w-full justify-center h-full">
+      <Game />
+    </div>
+  )
+}
