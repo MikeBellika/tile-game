9a3d177b4e987cca097c636be31e57c8ae78f853
Mike Bellika <mike@bellika.dk>
2024-04-03 16:40:17 +0200

    Improve settings modal

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -24,79 +24,90 @@ export default function Settings({
 
   const [open, setOpen] = useState(true)
   return (
-    <div>
+    <>
       <AnimatePresence>
         {open && (
-          <motion.dialog
-            initial={{ y: "100%", opacity: 0 }}
-            animate={{ y: "-50%", opacity: 100 }}
-            exit={{ y: "100%", opacity: 0 }}
-            open={open}
-            className="fixed top-[50%] z-50 h-min w-full translate-y-[-50%] rounded-lg border bg-black p-8 text-black sm:w-1/2  dark:text-white"
-          >
-            <h2 className="whitespace-nowrap text-lg font-semibold leading-none tracking-tight">
-              Settings
-            </h2>
-            <span className="text-muted-foreground mt-2 text-sm">
-              Control animation speed and game position.
-            </span>
+          <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 100 }}
+              exit={{ opacity: 0 }}
+              className="fixed inset-0 z-40 bg-opacity-75 backdrop-blur-sm transition-opacity"
+            />
+            <motion.dialog
+              initial={{ y: "100%", opacity: 0 }}
+              animate={{ y: "-100%", opacity: 100 }}
+              exit={{ y: "100%", opacity: 0 }}
+              transition={{ type: "tween", duration: 0.15 }}
+              open={open}
+              className="fixed z-50 h-min w-full rounded-lg border bg-black p-8 text-black sm:w-1/2  dark:text-white"
+            >
+              <h2 className="whitespace-nowrap text-lg font-semibold leading-none tracking-tight">
+                Settings
+              </h2>
+              <span className="text-muted-foreground mt-2 text-sm">
+                Control animation speed and game position.
+              </span>
 
-            <div className="mt-4 grid gap-1">
-              <h3 className="mb-1 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                Animation speed
-              </h3>
-              {Object.keys(AnimationSpeeds).map((speed) => (
-                <div key={speed}>
-                  <input
-                    type="radio"
-                    name="speed"
-                    id={`speed-${speed}`}
-                    className="peer hidden"
-                    checked={speed == animationSpeed}
-                    value={speed}
-                    onChange={() => setAnimationSpeed(speed as AnimationSpeed)}
-                  />
+              <div className="mt-4 grid gap-1">
+                <h3 className="mb-1 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  Animation speed
+                </h3>
+                {Object.keys(AnimationSpeeds).map((speed) => (
+                  <div key={speed}>
+                    <input
+                      type="radio"
+                      name="speed"
+                      id={`speed-${speed}`}
+                      className="peer hidden"
+                      checked={speed == animationSpeed}
+                      value={speed}
+                      onChange={() =>
+                        setAnimationSpeed(speed as AnimationSpeed)
+                      }
+                    />
 
-                  <label
-                    htmlFor={`speed-${speed}`}
-                    className="flex items-center gap-2 rounded border px-4 py-2 capitalize peer-checked:border-blue-500 peer-checked:bg-slate-200 peer-checked:dark:bg-slate-600"
-                  >
-                    {speed}
-                  </label>
-                </div>
-              ))}
-            </div>
-            <div className="mt-4 grid gap-1">
-              <h3 className="mb-1 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                Game position
-              </h3>
-              {GamePositions.map((_gamePosition) => (
-                <div key={_gamePosition}>
-                  <input
-                    type="radio"
-                    name="gamePosition"
-                    id={`gamePosition-${_gamePosition}`}
-                    className="peer hidden"
-                    checked={_gamePosition == gamePosition}
-                    value={_gamePosition}
-                    onChange={() =>
-                      setGamePosition(_gamePosition as GamePosition)
-                    }
-                  />
+                    <label
+                      htmlFor={`speed-${speed}`}
+                      className="flex items-center gap-2 rounded border px-4 py-2 capitalize peer-checked:border-blue-500 peer-checked:bg-slate-200 peer-checked:dark:bg-slate-600"
+                    >
+                      {speed}
+                    </label>
+                  </div>
+                ))}
+              </div>
+              <div className="mt-4 grid gap-1">
+                <h3 className="mb-1 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  Game position
+                </h3>
+                {GamePositions.map((_gamePosition) => (
+                  <div key={_gamePosition}>
+                    <input
+                      type="radio"
+                      name="gamePosition"
+                      id={`gamePosition-${_gamePosition}`}
+                      className="peer hidden"
+                      checked={_gamePosition == gamePosition}
+                      value={_gamePosition}
+                      onChange={() =>
+                        setGamePosition(_gamePosition as GamePosition)
+                      }
+                    />
 
-                  <label
-                    htmlFor={`gamePosition-${_gamePosition}`}
-                    className="flex items-center gap-2 rounded border px-4 py-2 capitalize peer-checked:border-blue-500 peer-checked:bg-slate-200 peer-checked:dark:bg-slate-600"
-                  >
-                    {_gamePosition}
-                  </label>
-                </div>
-              ))}
-            </div>
-            <Button className="mt-4 w-full" onClick={() => setOpen(false)}>
-              Close
-            </Button>
-          </motion.dialog>
+                    <label
+                      htmlFor={`gamePosition-${_gamePosition}`}
+                      className="flex items-center gap-2 rounded border px-4 py-2 capitalize peer-checked:border-blue-500 peer-checked:bg-slate-200 peer-checked:dark:bg-slate-600"
+                    >
+                      {_gamePosition}
+                    </label>
+                  </div>
+                ))}
+              </div>
+              <Button className="mt-4 w-full" onClick={() => setOpen(false)}>
+                Close
+              </Button>
+            </motion.dialog>
+          </div>
         )}
       </AnimatePresence>
 
@@ -106,6 +117,6 @@ export default function Settings({
       >
         Settings
       </button>
-    </div>
+    </>
   )
 }
