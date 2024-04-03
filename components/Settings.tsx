ba0c4cd435d62c8a440c595a86112735c385dfb4
Mike Bellika <mike@bellika.dk>
2024-04-03 17:44:31 +0200

    Remove option for game position for now

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -44,7 +44,7 @@ export default function Settings({
                 Settings
               </h2>
               <span className="text-muted-foreground mt-2 text-sm">
-                Control animation speed and game position.
+                Control animation speed
               </span>
 
               <div className="mt-4 grid gap-1">
@@ -74,33 +74,33 @@ export default function Settings({
                   </div>
                 ))}
               </div>
-              <div className="mt-4 grid gap-1">
-                <h3 className="mb-1 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                  Game position
-                </h3>
-                {GamePositions.map((_gamePosition) => (
-                  <div key={_gamePosition}>
-                    <input
-                      type="radio"
-                      name="gamePosition"
-                      id={`gamePosition-${_gamePosition}`}
-                      className="peer hidden"
-                      checked={_gamePosition == gamePosition}
-                      value={_gamePosition}
-                      onChange={() =>
-                        setGamePosition(_gamePosition as GamePosition)
-                      }
-                    />
+              {/* <div className="mt-4 grid gap-1"> */}
+              {/*   <h3 className="mb-1 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"> */}
+              {/*     Game position */}
+              {/*   </h3> */}
+              {/*   {GamePositions.map((_gamePosition) => ( */}
+              {/*     <div key={_gamePosition}> */}
+              {/*       <input */}
+              {/*         type="radio" */}
+              {/*         name="gamePosition" */}
+              {/*         id={`gamePosition-${_gamePosition}`} */}
+              {/*         className="peer hidden" */}
+              {/*         checked={_gamePosition == gamePosition} */}
+              {/*         value={_gamePosition} */}
+              {/*         onChange={() => */}
+              {/*           setGamePosition(_gamePosition as GamePosition) */}
+              {/*         } */}
+              {/*       /> */}
 
-                    <label
-                      htmlFor={`gamePosition-${_gamePosition}`}
-                      className="flex items-center gap-2 rounded border px-4 py-2 capitalize peer-checked:border-blue-500 peer-checked:bg-slate-200 peer-checked:dark:bg-slate-600"
-                    >
-                      {_gamePosition}
-                    </label>
-                  </div>
-                ))}
-              </div>
+              {/*       <label */}
+              {/*         htmlFor={`gamePosition-${_gamePosition}`} */}
+              {/*         className="flex items-center gap-2 rounded border px-4 py-2 capitalize peer-checked:border-blue-500 peer-checked:bg-slate-200 peer-checked:dark:bg-slate-600" */}
+              {/*       > */}
+              {/*         {_gamePosition} */}
+              {/*       </label> */}
+              {/*     </div> */}
+              {/*   ))} */}
+              {/* </div> */}
               <Button className="mt-4 w-full" onClick={() => setOpen(false)}>
                 Close
               </Button>
