c8d90402d7273102984d2e800a5d58546512f18d
Mike Bellika <mike@bellika.dk>
2024-04-03 16:41:27 +0200

    Fix lint error

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
-import dynamic from "next/dynamic"
+"use client"
+import Game from "@/components/Game"
+import Settings from "@/components/Settings"
+import { AnimationSpeeds, useSettings } from "@/hooks/useSettings"
 
 export default function Home() {
-  // To client side render the game
-  const Game = dynamic(() => import("../components/Game"), {
-    ssr: false,
-  })
+  const { animationSpeed, setAnimationSpeed, gamePosition, setGamePosition } =
+    useSettings()
+  const animationDuration = AnimationSpeeds[animationSpeed]
+
+  let justify = undefined
+  if (gamePosition == "top") {
+    justify = "justify-start"
+  } else if (gamePosition == "middle") {
+    justify = "justify-center"
+  } else {
+    justify = "justify-end"
+  }
+
   return (
-    <div className="flex h-full w-full justify-center">
-      <Game />
+    <div className="flex h-full w-full flex-col items-center">
+      <div className={`flex flex-1 flex-col items-center ${justify}`}>
+        <Game animationDuration={animationDuration} />
+      </div>
+      <Settings
+        setAnimationSpeed={setAnimationSpeed}
+        animationSpeed={animationSpeed}
+        gamePosition={gamePosition}
+        setGamePosition={setGamePosition}
+      />
     </div>
   )
 }
