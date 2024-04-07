4204add52ce0e73f54bf69f4ea07bcb4b58b5abe
Mike Bellika <mike@bellika.dk>
2024-04-07 19:56:10 +0200

    Fix next ssr error

--- app/shared/page.tsx
+++ app/shared/page.tsx
@@ -3,6 +3,9 @@ import { decodeStateFromURL } from "@/utils/sharing"
 import dynamic from "next/dynamic"
 
 export default function Home() {
+  if (typeof window == "undefined") {
+    return <div>Loading</div>
+  }
   const sharedState = decodeStateFromURL(window.location.search)
 
   const SvgGrid = dynamic(() => import("../../components/SvgGrid"), {
