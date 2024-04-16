8a09578e004a9d266a968fd6b5a74b63db7ada11
Mike Bellika <mike@bellika.dk>
2024-04-16 22:12:46 +0200

    Update shared thing

--- app/shared/page.tsx
+++ app/shared/page.tsx
@@ -1,20 +1,52 @@
 "use client"
-import { decodeStateFromURL } from "@/utils/sharing"
-import dynamic from "next/dynamic"
+import Button from "@/components/Button"
+import { useBoard } from "@/hooks/useBoard"
+import { getGameState, saveGameState } from "@/utils/storedState"
+import { redirect } from "next/navigation"
+import { useEffect, useState } from "react"
 
 export default function Home() {
   if (typeof window == "undefined") {
     return <div>Loading</div>
   }
-  const sharedState = decodeStateFromURL(window.location.search)
+  const sharedState = new URLSearchParams(window.location.search)
+  const seed = sharedState.get("s") ?? undefined
+  if (seed == undefined) {
+    redirect("/exponentile")
+  }
+  const { board: initialBoard, rng } = useBoard(8, seed)
 
-  const SvgGrid = dynamic(() => import("../../components/SvgGrid"), {
-    ssr: false,
-  })
+  const [loading, setLoading] = useState(true)
+
+  async function setStateFromSeed() {
+    const state = rng.state()
+    console.log("state:", state.j, state.i, state.S[0], state.S[1])
+    await saveGameState(initialBoard, 0, 0, rng.state())
+    window.location.href = "/exponentile"
+  }
+
+  useEffect(() => {
+    async function setBoard() {
+      const existingState = await getGameState()
+      // We don't want to overwrite the existing game
+      if (!existingState || existingState.moves == 0) {
+        await setStateFromSeed()
+      } else {
+        setLoading(false)
+      }
+    }
+    setBoard()
+  }, [])
 
   return (
-    <div className="flex h-full w-full justify-center">
-      {sharedState && <SvgGrid board={sharedState?.board} />}
+    <div className="flex h-full w-full flex-col items-center justify-center px-4">
+      {!loading && (
+        <div className="flex w-full flex-col items-center gap-6 sm:w-1/2">
+          You already have an ongoing game. Do you want to overwrite it and
+          start from scratch?
+          <Button onClick={() => setStateFromSeed()}>Ok</Button>
+        </div>
+      )}
     </div>
   )
 }
