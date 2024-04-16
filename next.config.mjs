271a138c2c268085a20e840a9aac9a9e6ec01990
Mike Bellika <mike@bellika.dk>
2024-04-16 21:36:38 +0200

    Disable strict mode to avoid rng being invoked twice

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  // When rerendering, the rng will be called again.
+  // Could probably fix this some better way, but it's getting late and this works
+  reactStrictMode: false
 };
 
 export default nextConfig;
